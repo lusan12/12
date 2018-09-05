1dc35166d9d2ca02e3d0709fc17f751e429e775b
LuSan <30853030+lusan12@users.noreply.github.com>
2018-09-04 21:52:30 -0500

    Add files via upload

---
+++ Figura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Areas
+{
+    abstract class Figura
+    {
+        protected double x;
+        protected double y;
+        protected double area;
+
+        public double x1 { get => x; set => x = value; }
+        public double y1 { get => y; set => y = value; }
+        public double Area { get => area; set => area = value; }
+        public abstract void CalcularArea();
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Areas
+{
+    class Program
+
+
+    {
+        static void Main(string[] args)
+        {
+            //Figura Figuras1 = new Figura();
+            int menu;
+            /*double x = 0;
+            double y = 0;
+            double area = 0;*/
+
+            Console.WriteLine("Seleciona la Figura \n1) Cuadrado\n2) Triangulo\n3) Circulo\n4");
+            menu = Convert.ToInt16(Console.ReadLine());
+
+            switch (menu)
+            {
+
+                case 1:
+                    Console.WriteLine("Escogiste cuadrado, dame el lado");
+                    string x = Console.ReadLine();
+                    double X = double.Parse(x);
+                    break;
+                /*case 2:
+                    Console.WriteLine("Escogiste Triangulo, dame la base y la altura");
+                    Figuras.CalcularArea();
+                    Figuras.CalcularArea();
+                    break;
+                case 3:
+                    Console.WriteLine("Escogiste Circulo, dame el radio");
+                    Figuras.CalcularArea();
+                    break;*/
+
+            }
+
+
+        }
+    }
+}
